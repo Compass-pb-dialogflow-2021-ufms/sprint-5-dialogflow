6f593c2c0adc558a1e28fa5c98ef4bb06d821cb5
Tiago Seiti Fugishima <tiago.fugishima@gmail.com>
2021-12-22 17:47:17 -0400

    Commit 0 - Preparação do projeto e estrutura básica para subir a aplicação.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Importar rotas.
+const root = require('./routes/root')
+
+// Rotas.
+app.use('/', root)
+
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}.`)
+})
---
+++ src/controllers/rootController.js
@@ -0,0 +1,7 @@
+const main = (req, res) => {
+
+}
+
+module.exports = {
+    main
+}
