a78a9d0a1bdc6d3631fb28a1ed421ef4b14610a3
Tiago Seiti Fugishima <tiago.fugishima@gmail.com>
2021-12-22 19:43:30 -0400

    Commit 1 - Switcher para as intents e fluxo de conversa paras as intents obrigatórias (Saudação, Despedida, Fallback).

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -1,5 +1,53 @@
-const main = (req, res) => {
+const switchIntent = (req, res, intent) => {
+    switch (intent) {
+        case 'Default Welcome Intent':
+            defaultWelcomeIntent(req, res)
+            break
+        case 'Default Fallback Intent':
+            defaultFallbackIntent(req, res)
+            break
+        case 'Farewell Intent':
+            farewellIntent(req, res)
+            break
+    }
+}
 
+const defaultWelcomeIntent = (req, res) => {
+    const response = {
+        fulfillmentText: [
+              'Olá ! Sou seu metereologista pessoal.\n\n'
+            + 'Posso dizer o clima de qualquer cidade do Brasil, quando precisar é só me chamar.'
+        ]
+    }
+
+    res.send(response)
+}
+
+const defaultFallbackIntent = (req, res) => {
+    const response = {
+        fulfillmentText: [
+            'Desculpe mas eu não trato desses assuntos.'
+        ]
+    }
+
+    res.send(response)
+}
+
+const farewellIntent = (req, res) => {
+    const response = {
+        fulfillmentText: [
+              'Sempre que precisar é só me chamar.\n'
+            + 'Até mais e lembre-se, se for sair leve um casaquinho.'
+        ]
+    }
+
+    res.send(response)
+}
+
+const main = (req, res) => {
+    const intent = req.body.queryResult.intent.displayName
+
+    switchIntent(req, res, intent)
 }
 
 module.exports = {
