8cc8c9a3861631a3b9b14b7ef68b758bda2ac265
hori-c-c <horielcoxim@gmail.com>
2021-12-24 17:35:55 -0400

    Intent de welcome adicionada

---
+++ dialogflow/intents/welcome.js
@@ -0,0 +1,9 @@
+function welcome()
+{
+    const text = `Olá, seja bem vindo. Eu sou a Maju.bot e posso te informar a previsão do tempo para os próximos 4 dias em qualquer município brasileiro! Voce gostaria de ver os dados meteriológicos de qual município?`
+
+    return text
+}
+
+
+module.exports = welcome
--- routs/roter.js
+++ routs/roter.js
@@ -1,10 +1,19 @@
 const router = require('express').Router()
+const formattedMessage = require('../dialogflow/responseModels/message')
+
+const welcome = require('../dialogflow/intents/welcome')
 
 
 router.post('', (req, res) => 
 {
     const intent = req.body.queryResult.intent.displayName
-    console.log(intent)
+    switch (intent) {
+        case 'Welcome':
+            res.send(formattedMessage(welcome()))
+            break
+        default:
+            break;
+    }
 })
 
 
