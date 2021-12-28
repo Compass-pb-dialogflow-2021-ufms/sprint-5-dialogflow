34b3bf6b6e040afa20d350f868bd4bb9bd2d3bc7
hori-c-c <horielcoxim@gmail.com>
2021-12-28 19:23:28 -0400

    Fazendo conexão ao banco de dados

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const router = require('./routs/router')
+const mongoose = require('mongoose')
 
 
 const app = express()
@@ -10,5 +12,17 @@ app.use(express.urlencoded({extended: true}))
 app.use('/assistencia', router)
 
 
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const url = process.env.MONGODB_URI || `mongodb+srv://${DB_USER}:${DB_PASSWORD}@assistenciatecnica.gf9jz.mongodb.net/mrRobotDataBase?retryWrites=true&w=majority`
 const port = process.env.PORT || 3000
-app.listen(port, () => {console.log(`O servidor rodando na porta ${port}`)})
+
+
+mongoose.connect(url)
+.then(() => {
+    console.log('Conectado ao banco de dados')
+    app.listen(port, () => {console.log(`O servidor rodando na porta ${port}`)})
+})
+.catch((erro) => {
+    console.log(erro)
+})
